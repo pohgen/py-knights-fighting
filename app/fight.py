1abf49eb0d557eb989d6c0a82fbf1c85248b5a98
Alex <delete.rap.stupid@gmail.com>
2025-03-19 14:19:25 +0100

    Revert "solution"
    This reverts commit 69ec592dbd75f05b78948343cb066765b7793ff2.

--- app/fight.py
+++
@@ -1,8 +0,0 @@
-from app.knights import Knight
-
-
-def fight(knight_1: Knight, knight_2: Knight) -> dict:
-    knight_1.hp = max(0, knight_1.hp - (knight_2.power - knight_1.armour))
-    knight_2.hp = max(0, knight_2.hp - (knight_1.power - knight_2.armour))
-
-    return {knight_1.name: knight_1.hp, knight_2.name: knight_2.hp}
